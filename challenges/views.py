1e74e67f455c37f2b0c71a1d181baf5ea86ae26e
Veniamin Polienko <kasaklalita@mail.ru>
2022-02-01 02:42:41 +0300

    added a dynamic segment path for months

--- challenges/views.py
+++ challenges/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse("Eat no meat for the entire month!")
-
-def february(request):
-    return HttpResponse("Work for at least 20 minutes every day!")
+def monthly_challenge(request, month): #<month> from urls.py
+    challenge_text = None
+    if month == "january":
+        challenge_text = "Eat no meat for the entire month!"
+    elif month == "february":
+        challenge_text = "Work for at least 20 minutes every day!"
+    elif month == "march":
+        challenge_text = "Learn Django for at least 20 minutes every day!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
